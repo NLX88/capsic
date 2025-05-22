dde4f44f75ec0e32cb94a831a62f9650cc3ad3af
ernesto <netorh3001@gmail.com>
2025-05-22 17:12:43 -0600

    b -2.0 (fadein+responsive)

--- scripts.js
+++ scripts.js
@@ -59,3 +59,21 @@ document.addEventListener("DOMContentLoaded", () => {
         });
     });
 });
+
+/*DESVANECIMEINTO*/
+document.addEventListener("DOMContentLoaded", () => {
+    const elementos = document.querySelectorAll('.fade-in');
+
+    const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('visible');
+                observer.unobserve(entry.target);
+            }
+        });
+    }, {
+        threshold: 0.2
+    });
+
+    elementos.forEach(el => observer.observe(el));
+});
